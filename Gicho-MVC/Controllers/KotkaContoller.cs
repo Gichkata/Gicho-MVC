7468dff16306c81211f1fd64a0069c94e7c3363f
Gichkata <118356405+Gichkata@users.noreply.github.com>
2023-01-20 09:20:20 +0200

    MVC

---
+++ Gicho-MVC/Controllers/KotkaContoller.cs
@@ -0,0 +1,17 @@
+﻿using Gicho_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gicho_MVC.Controllers
+{
+    public class KotkaContoller : Controller
+    {
+        public IActionResult index()
+        {
+            List<Kotka> Kotki = new List<Kotka>()
+            {
+                new Kotka() { KotkaId = 2, Poroda = "britanska", Vid = "malka", YearBorn = 2018}
+            };
+            return View(Kotki);
+        }
+    }
+}
---
+++ Gicho-MVC/Models/Kotka.cs
@@ -0,0 +1,11 @@
+﻿namespace Gicho_MVC.Models
+{
+    public class Kotka
+    {
+        public int KotkaId { get; set; }
+
+        public string Poroda { get; set; }
+        public string Vid { get; set; }
+        public int YearBorn { get; set; }
+    }
+}
